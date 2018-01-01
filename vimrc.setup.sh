e6a366bd7fdb3580d8afde61b2287f8a8bf2e0a8
Rolf Pfeffertal <tropf@users.noreply.github.com>
2018-01-01 15:01:06 +0100

    *cmake install

--- vimrc.setup.sh
+++ vimrc.setup.sh
@@ -7,9 +7,10 @@ bash
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-echo "#######################################################";
-echo "# Please use this shell to install clang and libboost #";
-echo "#######################################################";
+echo "##############################################################";
+echo "# Please use this shell to install cmake, clang and libboost #";
+echo "# (additionally build-essential on ubuntu)                   #";
+echo "##############################################################";
 
 bash
 
