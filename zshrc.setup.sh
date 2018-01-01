45d3bb64a244fb94221e0bf0bbea9d3b2b75be72
Rolf Pfeffertal <tropf@users.noreply.github.com>
2018-01-01 15:15:55 +0100

    +zshrc setup prototype

---
+++ zshrc.setup.sh
@@ -0,0 +1,9 @@
+echo "###############################################";
+echo "# Please use this shell to install zsh & wget #";
+echo "###############################################";
+
+bash
+
+echo "installing Oh-My-ZSH...";
+
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
