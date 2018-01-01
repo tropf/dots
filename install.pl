b9f1f9dc9be454f58619fb9583d20f8960bf2f95
Rolf Pfeffertal <foo@bar>
2018-01-01 11:14:47 +0100

    *include marked with comments

--- install.pl
+++ install.pl
@@ -13,41 +13,72 @@ use File::HomeDir;
 my $homedir = File::HomeDir->my_home;
 my $dirname = abs_path(dirname(__FILE__));
 
+my $template_filename = $dirname . "/zshrc.inc";
+my $rc_included_filename = $dirname . "/zshrc";
+my $rc_actual_filename = $homedir . "/.zshrc";
+my $comment_filename = $dirname . "/zshrc.comment";
+
+# load comment template
+my $comment_fh = IO::File->new();
+$comment_fh->open("< $comment_filename");
+my $comment_temmplate = <$comment_fh>;
+$comment_fh->close;
+
+$comment_temmplate =~ s/\R$//;
+$comment_temmplate .= "\n";
+
+# generate start & end comments
+my $comment_include_block_start = $comment_temmplate =~ s/\$COMMENT/***** BEGIN INCLUDE BLOCK *****/r;
+my $comment_include_block_end = $comment_temmplate =~ s/\$COMMENT/***** END INCLUDE BLOCK *****/r;
+
 # load include script template
 my $inc_fh = IO::File->new();
-$inc_fh->open("< $dirname/zshrc.inc");
+$inc_fh->open("< $template_filename");
 my @inc_template_lines = <$inc_fh>;
 $inc_fh->close;
 my $inc_template = join '', @inc_template_lines;
 
 # generate include script
-my $rcfile = 'path/to/rcfile';
-my $inc_text = $inc_template =~ s/\$FILE/$rcfile/r;
-
-print $inc_text;
+my $inc_text = $inc_template =~ s/\$FILE/$rc_included_filename/r;
 
 # load current .rc
 my $rc_fh = IO::File->new();
-$rc_fh->open("< $homedir/.zshrc") or die("didn't find .zshrc file");
+$rc_fh->open("< $rc_actual_filename") or die("didn't find .zshrc file");
 my @rc_old_lines = <$rc_fh>;
 $rc_fh->close;
 
 # filter old include script from .rc
-my @rc_new_lines = grep(!/$inc_text/, @rc_old_lines);
+my @rc_new_lines;
+my $keep_copying = 1;
+# search for **** BEGIN INCLUDE BLOCK ***** and don't copy it
+my $line;
+foreach $line (@rc_old_lines) {
+    if ($line =~ s/\s*\R?$//r eq $comment_include_block_start =~ s/\s*\R?$//r) {
+        $keep_copying = 0;
+    }
+
+    if ($keep_copying) {
+        push @rc_new_lines, $line;
+    }
+
+    if ($line =~ s/\s*\R?$//r eq $comment_include_block_end =~ s/\s*\R?$//r) {
+        $keep_copying = 1;
+    }
+}
 
 # put new include script on top of .rc
 my $rc_new = join '', @rc_new_lines;
 
 # kill (eventually) trailing \n
 $inc_text =~ s/\R$//;
+$inc_text .= "\n";
+$inc_text = $comment_include_block_start . $inc_text . $comment_include_block_end;
+
 $rc_new =~ s/\R$//;
-$rc_new = $inc_text . "\n" . $rc_new . "\n";
+$rc_new = $inc_text . $rc_new . "\n";
 
 # .rc write back
 my $rc_wb_fh = IO::File->new();
-$rc_wb_fh->open("> /tmp/.zshrc") or die("No writerino to .zshrc file");
-if (! defined $rc_wb_fh) {
-    die('shit');
-}
+$rc_wb_fh->open("> $rc_actual_filename") or die("No writerino to .zshrc file");
 print $rc_wb_fh $rc_new;
 $rc_wb_fh->close;
