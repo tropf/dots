a37e8bc144891f7f537690386e3e80e198a870fc
Rolf Pfeffertal <tropf@users.noreply.github.com>
2018-01-01 14:58:01 +0100

    +stuff for vim

--- vimrc.setup.sh
+++ vimrc.setup.sh
@@ -6,3 +6,10 @@ echo "###############################################"
 bash
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "#######################################################";
+echo "# Please use this shell to install clang and libboost #";
+echo "#######################################################";
+
+bash
+
--- vimrc.update.sh
+++ vimrc.update.sh
@@ -1,2 +1,5 @@
 vim +PluginInstall +qall
 vim +PluginUpdate +qall
+
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer --system-libclang --system-boost
