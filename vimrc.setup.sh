209cccb01894e0ff9389d1ebe3bf47ad23d7a797
Rolf Pfeffertal <tropf@users.noreply.github.com>
2018-01-01 15:04:58 +0100

    *other python reminder

--- vimrc.setup.sh
+++ vimrc.setup.sh
@@ -9,7 +9,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "##############################################################";
 echo "# Please use this shell to install cmake, clang and libboost #";
-echo "# as well as python (python3)                                #";
+echo "# as well as python with headers (python-dev python3-dev)    #";
 echo "# (additionally build-essential on ubuntu)                   #";
 echo "##############################################################";
 
