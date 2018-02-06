bd48ac7bf168acdc92a829601b69873054d03ef9
Rolf Pfeffertal <tropf@users.noreply.github.com>
2018-02-06 14:43:08 +0100

    *update script now accepts entries w/ only scripts, +st in conf

--- update.pl
+++ update.pl
@@ -44,149 +44,165 @@ my $conf_entry;
 for $conf_entry (@conf) {
     my (%current_conf) = %{$conf_entry};
 
-    my $template_filename = $dirname . "/" . $current_conf{"inc"};
-    my $rc_included_filename = $dirname . "/" . $current_conf{"rc"};
-    my $rc_actual_filename = $homedir . "/" . $current_conf{"target"};
-    my $comment_filename = $dirname . "/" . $current_conf{"comment"};
+    # the keys
+    # inc, rc, target, comment
+    # always exist together: if one exists, all exist
+    # -> check that
+    
+    if (exists $current_conf{"inc"} &&
+        exists $current_conf{"rc"} &&
+        exists $current_conf{"target"} &&
+        exists $current_conf{"comment"}) {
+        # all files exist, do file checks
 
-    print "Checking files for $current_conf{target}\n";
-    my $files_ok = 1;
+        my $template_filename = $dirname . "/" . $current_conf{"inc"};
+        my $rc_included_filename = $dirname . "/" . $current_conf{"rc"};
+        my $rc_actual_filename = $homedir . "/" . $current_conf{"target"};
+        my $comment_filename = $dirname . "/" . $current_conf{"comment"};
 
-    print "  " . $template_filename . "...";
-    if (-e $template_filename ) {
-        if (-r $template_filename) {
-            print "OK\n";
-        } else {
-            print "NOT READABLE\n";
-            $files_ok = 0;
-        }
-    } else {
-        print "MISSING\n";
-        $files_ok = 0;
-    }
+        print "Checking files for $current_conf{target}\n";
+        my $files_ok = 1;
 
-    print "  " . $rc_included_filename . "...";
-    if (-e $rc_included_filename ) {
-        if (-r $rc_included_filename) {
-            print "OK\n";
-        } else {
-            print "NOT READABLE\n";
-            $files_ok = 0;
-        }
-    } else {
-        print "MISSING\n";
-        $files_ok = 0;
-    }
-
-    print "  " . $rc_actual_filename . "...";
-    system("touch", $rc_actual_filename);
-    if (-e $rc_actual_filename) {
-        if (-r $rc_actual_filename) {
-            if (-w $rc_actual_filename) {
+        print "  " . $template_filename . "...";
+        if (-e $template_filename ) {
+            if (-r $template_filename) {
                 print "OK\n";
             } else {
-                print "NOT WRITEABLE\n";
+                print "NOT READABLE\n";
                 $files_ok = 0;
             }
         } else {
-            print "NOT READABLE\n";
+            print "MISSING\n";
             $files_ok = 0;
         }
-    } else {
-        print "MISSING\n";
-        $files_ok = 0;
-    }
 
-    print "  " . $comment_filename . "...";
-    if (-e $comment_filename) {
-        if (-r $comment_filename) {
-            print "OK\n";
+        print "  " . $rc_included_filename . "...";
+        if (-e $rc_included_filename ) {
+            if (-r $rc_included_filename) {
+                print "OK\n";
+            } else {
+                print "NOT READABLE\n";
+                $files_ok = 0;
+            }
         } else {
-            print "NOT READABLE\n";
+            print "MISSING\n";
             $files_ok = 0;
         }
-    } else {
-        print "MISSING\n";
-        $files_ok = 0;
-    }
 
-    if (! $files_ok) {
-        print "  >>> file errors, skipping\n";
-        next;
-    }
-
-    # load comment template
-    my $comment_fh = IO::File->new();
-    $comment_fh->open("< $comment_filename");
-    my $comment_temmplate = <$comment_fh>;
-    $comment_fh->close;
-
-    $comment_temmplate =~ s/\R$//;
-    $comment_temmplate .= "\n";
-
-    # generate start & end comments
-    my $comment_include_block_start = $comment_temmplate =~ s/\$COMMENT/***** BEGIN INCLUDE BLOCK *****/r;
-    my $comment_include_block_end = $comment_temmplate =~ s/\$COMMENT/***** END INCLUDE BLOCK *****/r;
-
-    # load include script template
-    my $inc_fh = IO::File->new();
-    $inc_fh->open("< $template_filename");
-    my @inc_template_lines = <$inc_fh>;
-    $inc_fh->close;
-    my $inc_template = join '', @inc_template_lines;
-
-    # generate include script
-    my $inc_text = $inc_template =~ s/\$FILE/$rc_included_filename/r;
-
-    # load current .rc
-    my $rc_fh = IO::File->new();
-    $rc_fh->open("< $rc_actual_filename") or die("didn't find .zshrc file");
-    my @rc_old_lines = <$rc_fh>;
-    $rc_fh->close;
-
-    # filter old include script from .rc
-    my @rc_new_lines;
-    my $keep_copying = 1;
-
-    # search for **** BEGIN INCLUDE BLOCK ***** and don't copy it
-    my $line;
-    foreach $line (@rc_old_lines) {
-        if ($line =~ s/\s*\R?$//r eq $comment_include_block_start =~ s/\s*\R?$//r) {
-            $keep_copying = 0;
+        print "  " . $rc_actual_filename . "...";
+        system("touch", $rc_actual_filename);
+        if (-e $rc_actual_filename) {
+            if (-r $rc_actual_filename) {
+                if (-w $rc_actual_filename) {
+                    print "OK\n";
+                } else {
+                    print "NOT WRITEABLE\n";
+                    $files_ok = 0;
+                }
+            } else {
+                print "NOT READABLE\n";
+                $files_ok = 0;
+            }
+        } else {
+            print "MISSING\n";
+            $files_ok = 0;
         }
 
-        if ($keep_copying) {
-            push @rc_new_lines, $line;
+        print "  " . $comment_filename . "...";
+        if (-e $comment_filename) {
+            if (-r $comment_filename) {
+                print "OK\n";
+            } else {
+                print "NOT READABLE\n";
+                $files_ok = 0;
+            }
+        } else {
+            print "MISSING\n";
+            $files_ok = 0;
         }
 
-        if ($line =~ s/\s*\R?$//r eq $comment_include_block_end =~ s/\s*\R?$//r) {
-            $keep_copying = 1;
+        if (! $files_ok) {
+            print "  >>> file errors, skipping\n";
+            next;
         }
+
+        # load comment template
+        my $comment_fh = IO::File->new();
+        $comment_fh->open("< $comment_filename");
+        my $comment_temmplate = <$comment_fh>;
+        $comment_fh->close;
+
+        $comment_temmplate =~ s/\R$//;
+        $comment_temmplate .= "\n";
+
+        # generate start & end comments
+        my $comment_include_block_start = $comment_temmplate =~ s/\$COMMENT/***** BEGIN INCLUDE BLOCK *****/r;
+        my $comment_include_block_end = $comment_temmplate =~ s/\$COMMENT/***** END INCLUDE BLOCK *****/r;
+
+        # load include script template
+        my $inc_fh = IO::File->new();
+        $inc_fh->open("< $template_filename");
+        my @inc_template_lines = <$inc_fh>;
+        $inc_fh->close;
+        my $inc_template = join '', @inc_template_lines;
+
+        # generate include script
+        my $inc_text = $inc_template =~ s/\$FILE/$rc_included_filename/r;
+
+        # load current .rc
+        my $rc_fh = IO::File->new();
+        $rc_fh->open("< $rc_actual_filename") or die("didn't find .zshrc file");
+        my @rc_old_lines = <$rc_fh>;
+        $rc_fh->close;
+
+        # filter old include script from .rc
+        my @rc_new_lines;
+        my $keep_copying = 1;
+
+        # search for **** BEGIN INCLUDE BLOCK ***** and don't copy it
+        my $line;
+        foreach $line (@rc_old_lines) {
+            if ($line =~ s/\s*\R?$//r eq $comment_include_block_start =~ s/\s*\R?$//r) {
+                $keep_copying = 0;
+            }
+
+            if ($keep_copying) {
+                push @rc_new_lines, $line;
+            }
+
+            if ($line =~ s/\s*\R?$//r eq $comment_include_block_end =~ s/\s*\R?$//r) {
+                $keep_copying = 1;
+            }
+        }
+
+        # put new include script on top of .rc
+        my $rc_new = join '', @rc_new_lines;
+
+        # kill (possibly) trailing \n
+        $inc_text =~ s/\R$//;
+        $inc_text .= "\n";
+        $inc_text = $comment_include_block_start . $inc_text . $comment_include_block_end;
+
+        $rc_new =~ s/\R$//;
+        $rc_new = $rc_new . "\n";
+
+        if (!$opts{r}) {
+            print "  injecting include script\n";
+            $rc_new = $rc_new . $inc_text;
+        } else {
+            print "  removing include block\n";
+        }
+
+        # .rc write back
+        my $rc_wb_fh = IO::File->new();
+        $rc_wb_fh->open("> $rc_actual_filename") or die("No writerino to .zshrc file");
+        print $rc_wb_fh $rc_new;
+        $rc_wb_fh->close;
     }
 
-    # put new include script on top of .rc
-    my $rc_new = join '', @rc_new_lines;
-
-    # kill (possibly) trailing \n
-    $inc_text =~ s/\R$//;
-    $inc_text .= "\n";
-    $inc_text = $comment_include_block_start . $inc_text . $comment_include_block_end;
-
-    $rc_new =~ s/\R$//;
-    $rc_new = $rc_new . "\n";
-
-    if (!$opts{r}) {
-        print "  injecting include script\n";
-        $rc_new = $rc_new . $inc_text;
-    } else {
-        print "  removing include block\n";
-    }
-
-    # .rc write back
-    my $rc_wb_fh = IO::File->new();
-    $rc_wb_fh->open("> $rc_actual_filename") or die("No writerino to .zshrc file");
-    print $rc_wb_fh $rc_new;
-    $rc_wb_fh->close;
+    # two options to get execution to this point
+    # a) script injection successfull
+    # b) no script to inject, there is only the update script
 
     # execute update script
     if (exists $current_conf{"update"}) {
