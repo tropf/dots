d2eda0d45083e8973ffc58f7022e3e42c9de1f04
Rolf Pfeffertal <tropf@users.noreply.github.com>
2018-02-06 14:30:04 +0100

    *complete setup script for st, works with headless now

--- st/setup.sh
+++ st/setup.sh
@@ -1,14 +1,12 @@
-echo "##########################################";
-echo "# X Terminal installation                #";
-echo "# Press [ENTER] to continue (desktop PC) #";
-echo "# Press CTRL-C to abort (server, ssh)    #"
-echo "##########################################";
+echo "################################";
+echo "# Simple Terminal - Step 1     #";
+echo "# Installing Terminfo files    #";
+echo "# (required for tmux etc.)     #";
+echo "# Downloading source & patches #";
+echo "################################";
 
-read entered;
-
-echo "Continuing installation...";
-
-echo "Downloading source & patches";
+echo "cleaning up old files...";
+xargs rm -r < .gitignore
 
 wget https://dl.suckless.org/st/st-0.7.tar.gz;
 wget https://st.suckless.org/patches/clipboard/st-clipboard-0.7.diff
@@ -29,6 +27,24 @@ patch -i ../st-visualbell-20160727-308bfbf.diff
 make config.h
 
 patch -i ../mine.patch
+
+echo "installing terminfo locally...";
+tic -s st-0.7/st.info
+
+echo "installing terminfo globally... (you can abort this step)";
+sudo tic -s st-0.7/st.info
+
+echo "##########################################";
+echo "# Simple Terminal - Step 2               #";
+echo "# Installing GUI                         #";
+echo "##########################################";
+echo "# X Terminal installation                #";
+echo "# Press [ENTER] to continue (desktop PC) #";
+echo "# Press CTRL-C to abort (server, ssh)    #"
+echo "##########################################";
+
+read entered;
+echo "Continuing installation...";
 
 echo "need password to install...";
 sudo make install
