6c392d74e4435efdee8103304972c0a95179187b
Rolf Pfeffertal <tropf@users.noreply.github.com>
2018-05-07 18:43:20 +0200

    *skeleton setup script

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+import json
+
+dirname = os.path.dirname(os.path.realpath(__file__))
+
+conf_filename = dirname + ="/conf.json"
+with open(conf_filename, 'r') as infile:
+    conf = json.loads(infile.read())
+
+for conf_entry in conf:
+    print('=== {} ==='.format(conf_entry["target"]))
+
+    if 'setup' in conf_entry:
+        setup_script_file = dirname + "/" + conf_entry['setup']
+        print("  running setup script {}".format(setup_script_file))
+
+        if os.path.isfile(setup_script_file) and os.access(setup_script_file, os.X_OK):
+            os.system(setup_script_file)
+    else:
+        print ("  no setup script, skipping")
+
+import .update
